16db3b24bfadccfe0b007ff6f8613034ded234e7
Kauany Miranda <kauany.m.dantas@gmail.com>
2024-11-27 19:21:00 -0400

    Inicio do projeto

---
+++ Cliente.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+using static System.Convert;
+public class Cliente
+{
+    public string _nome;
+    private string _cpf;
+    public string _sexo;
+    public string _dataNascimento;
+
+    public void GetCpf(string c)
+    {
+         c = c.Replace(".", "").Replace("-", "");
+
+        if (c.Length == 11)
+        {
+            _cpf = c;
+        }
+        else
+        {
+            throw new Exception("CPF inválido");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+using static System.Convert;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Cliente c = new Cliente();
+            Write("Digite o nome: ");
+            c._nome = ReadLine();
+            Write("Digite o cpf: ");
+            string cpf = ReadLine();
+            c.GetCpf(cpf);
+            Write("Digite o sexo: ");
+            c._sexo = ReadLine();
+            Write("Digite a Data de nascimento: ");
+            c._dataNascimento = ReadLine();
+        }
+        catch (Exception ex) 
+        { 
+            WriteLine(ex.Message);
+        }
+
+    }
+}
